20ba6cce01df9adecc4bb13c732b6c111e1024b1
Noeypatt <patnarin241240@gmail.com>
2022-09-15 02:17:13 +0700

    :tada: submissions with NextJS & Google Sheets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,32 @@
+import { Box, Button } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const handleSubmit = async () => {
+    let form = {
+      email: 'test03@gmail.com',
+      bitkub_account: true,
+      phone: '0987654321',
+      waitlist: true,
+      subscribe: false,
+    }
+
+    const rawResponse = await fetch('/api/submit', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    })
+    const content = await rawResponse.json()
+
+    alert(content.data.tableRange)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +36,18 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <Button variant="contained" onClick={handleSubmit}>
+            Submit Form
+          </Button>
+        </Box>
       </main>
 
       <footer className={styles.footer}>
